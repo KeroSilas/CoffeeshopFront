2901bd8919f162c01c18c9056269d909c56c35b0
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-12-04 11:21:41 +0100

    Login and Signup component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import {AddCoffeeComponent} from "./add-coffee/add-coffee.component";
 import {CoffeeDetailComponent} from "./coffee-detail/coffee-detail.component";
+import {SignupComponent} from "./signup/signup.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: 'coffee', pathMatch: 'full' },
   { path: 'coffee', component: AddCoffeeComponent },
   { path: 'coffee/:id', component: CoffeeDetailComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' }
 ];
