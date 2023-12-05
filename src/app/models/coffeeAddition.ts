efc7e61eebf26cbce1f024fc0da6f05e4a54ee0c
chrsut01 <chrissu360@gmail.com>
2023-12-05 10:20:01 +0100

    more added models

---
+++ src/app/models/coffeeAddition.ts
@@ -0,0 +1,5 @@
+export class CoffeeAdditionModel {
+    id = "";
+    customCoffeeId = "";
+    additionId = "";
+}
---
+++ src/app/models/orderDetailsModel.ts
@@ -0,0 +1,9 @@
+export class OrderDetailsModel {
+    id = "";
+    orderId = "";
+    predefinedCoffeeId = "";
+    customCoffeeId = "";
+    cookieId = "";
+    price = 0;
+    quantity = 0;
+}
