9cd7ba7cfa6b1e6628848c9e81989897499c5794
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-12-10 03:05:53 +0100

    Comment section styling

--- src/app/components/coffee-detail/coffee-detail.component.ts
+++ src/app/components/coffee-detail/coffee-detail.component.ts
@@ -13,6 +13,8 @@ import {CommentService} from "../../services/comment.service";
 import {CommentModel} from "../../models/comment.model";
 import {UserStateService} from "../../services/user-state.service";
 import {Guid} from "guid-typescript";
+import {UserService} from "../../services/user.service";
+import {NgArrayPipesModule} from "ngx-pipes";
 
 @Component({
   selector: 'app-coffee-detail',
@@ -23,10 +25,11 @@ import {Guid} from "guid-typescript";
     RouterLink,
     RouterLinkActive,
     MatIconModule,
+    NgArrayPipesModule,
   ],
   templateUrl: './coffee-detail.component.html',
   styleUrl: './coffee-detail.component.css',
-  providers: [CoffeeService, CommentService],
+  providers: [CoffeeService, CommentService, UserService],
 })
 export class CoffeeDetailComponent implements OnInit {
   coffee: PredefinedCoffeeModel = new PredefinedCoffeeModel();
@@ -38,9 +41,13 @@ export class CoffeeDetailComponent implements OnInit {
     private service: CoffeeService,
     private commentService: CommentService,
     private userState: UserStateService,
+    private userService: UserService,
   ) {}
 
   ngOnInit(): void {
+    if (!this.userState.isLoggedIn()) {
+      this.router.navigate(['login']).then((r) => console.log(r));
+    }
     let coffeeId = this.router.url.split('/')[2];
     this.service
       .getCoffeeById(coffeeId)
@@ -62,10 +69,20 @@ export class CoffeeDetailComponent implements OnInit {
     commentModel.predefinedCoffeeId = this.coffee.id;
     commentModel.userId = this.currentUserId;
     commentModel.commentTime = new Date();
-    this.commentService.createComment(commentModel).subscribe((data: CommentModel) => {
-      this.comments.push(data);
-    });
-    console.log(commentModel);
-    console.log(this.comments);
+    this.commentService
+      .createComment(commentModel)
+      .subscribe((data: CommentModel) => {
+        this.comments.push(data);
+      });
+  }
+
+  deleteComment(commentId: string) {
+    this.commentService
+      .deleteComment(commentId)
+      .subscribe((data: CommentModel) => {
+        this.comments = this.comments.filter(
+          (comment) => comment.id !== commentId,
+        );
+      });
   }
 }
