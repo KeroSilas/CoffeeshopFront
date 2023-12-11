f0066d497dc202644ed61e61a152741f14fefedc
chrsut01 <chrissu360@gmail.com>
2023-12-11 10:52:39 +0100

    added shopping-cart component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,7 @@ import {AdminDashboardComponent} from "./components/admin-dashboard/admin-dashbo
 import {AdminOrdersComponent} from "./components/admin-orders/admin-orders.component";
 import {AdminCoffeesComponent} from "./components/admin-coffees/admin-coffees.component";
 import {AdminUsersComponent} from "./components/admin-users/admin-users.component";
-//import { ShoppingCartComponent } from "./components/cart/cart.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
 
 export let routes: Routes;
 routes = [
@@ -15,7 +15,7 @@ routes = [
   {path: 'coffee', component: CoffeeListComponent},
   {path: 'coffee/:id', component: CoffeeDetailComponent},
   //{ path: 'coffee/create', component: CustomCoffeeComponent }, // Create custom coffee
-  //{ path: 'cart', component: ShoppingCartComponent }, // Shopping cart details
+  { path: 'cart', component: ShoppingCartComponent }, // Shopping cart details
   //{ path: 'order', component: OrderComponent }, // Ordering process
   {path: 'admin', component: AdminDashboardComponent}, // Overview of upcoming orders, etc.
   {path: 'admin/orders', component: AdminOrdersComponent}, // List of orders
