d1e91c3c0d820912be68900531c6582b5bc99981
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2023-12-12 18:25:01 +0100

    Stuff

---
+++ src/app/services/location.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {PickupLocationModel} from "../models/pickupLocation.model";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocationService {
+  private baseUrl = 'http://localhost:5245/api/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getPickupLocations(): Observable<PickupLocationModel[]> {
+    return this.http.get<PickupLocationModel[]>(this.baseUrl + 'GetPickupLocations');
+  }
+}
